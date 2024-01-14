414303e09ebfe9e4d6803fe5b3a42ea5d881c472
pa1zore <pavanzore345@gmail.com>
2024-01-14 23:44:52 +0530

    Added ability  to find the cheapest hotel for given date range

--- src/main/java/Hotel_Resevation_System_Main.java
+++ src/main/java/Hotel_Resevation_System_Main.java
@@ -1,11 +1,15 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-//creating hotel1 class for first adding multiple hotels
+
 class Hotel
 {
     //declearing data members as private for avoiding direct acess
     private String name;
     private int rateForRegularCustomer;
+    public Hotel()
+    {
+
+    }
     //parameterized constructor to initialised object
     public Hotel(String name, int rateForRegularCustomer)
     {
@@ -20,6 +24,10 @@ class Hotel
     {
         return this.rateForRegularCustomer;
     }
+    public void setRateForRegularCustomer(int rateForRegularCustomer)
+    {
+        this.rateForRegularCustomer=rateForRegularCustomer;
+    }
     // toString method for getting all the contents of current object
     public String toString()
     {
@@ -27,13 +35,53 @@ class Hotel
     }
 
 }
+
+
 public class Hotel_Resevation_System_Main {
-    static ArrayList<Hotel>  hotel = new ArrayList<>();
+    static ArrayList<Hotel> hotel = new ArrayList<>();
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        Hotel h=  new Hotel("Lakewood",110);
-        hotel.add(h);
-        System.out.println(h);
-    }
+        Hotel h1=  new Hotel("Lakewood",110);
+        Hotel h2=  new Hotel("Bredgewood",160);
+        Hotel h3=  new Hotel("Ringwood",220);
+        hotel.add(h1);
+        hotel.add(h2);
+        hotel.add(h3);
 
+        int dcount=0;
+        System.out.println("enter date");
+        String date1=sc.nextLine();
+        if(date1.matches("\\w"))
+            dcount++;
+        System.out.println("enter date");
+        String date2=sc.nextLine();
+        dcount++;
+
+        Hotel h;
+        h=cheapestHotel(dcount);
+        System.out.println(" cheapest hotel is");
+        System.out.println("Hotel name: "+h.getName()+" hotel price: "+h.getRateForRegularCustomer());
+
+
+    }
+    public static Hotel cheapestHotel(int count)
+    {
+        Hotel temp= new Hotel(hotel.get(0).getName(),hotel.get(0).getRateForRegularCustomer());
+        for (int i=0;i<hotel.size();i++)
+        {
+            if(temp.getRateForRegularCustomer()>hotel.get(i).getRateForRegularCustomer())
+            {
+                temp = new Hotel(hotel.get(i).getName(),hotel.get(i).getRateForRegularCustomer());
+            }
+        }
+        int i=0;
+        int sum=0;
+        while(i<count)
+        {
+            sum+=temp.getRateForRegularCustomer();
+            i++;
+        }
+        temp.setRateForRegularCustomer(sum);
+        return temp;
+    }
 }
--- src/test/java/Hotel_Reservation_System_Test.java
+++ src/test/java/Hotel_Reservation_System_Test.java
@@ -18,4 +18,18 @@ public class Hotel_Reservation_System_Test {
         Assertions.assertEquals(actualName,name);
         Assertions.assertEquals(actualPrice,price);
     }
+    @Test
+    void should_return_chepeast_hotel_name_and_price()
+    {
+        Hotel h1=  new Hotel("Lakewood",110);
+        Hotel h2=  new Hotel("Bredgewood",160);
+        Hotel h3=  new Hotel("Ringwood",220);
+        Hotel_Resevation_System_Main.hotel.add(h1);
+        Hotel_Resevation_System_Main.hotel.add(h2);
+        Hotel_Resevation_System_Main.hotel.add(h3);
+
+        Hotel h=Hotel_Resevation_System_Main.cheapestHotel(2);
+        int actual=h.getRateForRegularCustomer();
+        Assertions.assertEquals(actual,220);
+    }
 }
