8c61fc59962d61747ffaed46d7e25288d898e245
pa1zore <pavanzore345@gmail.com>
2024-01-14 23:05:57 +0530

    Ability to add hotel in hotel reservation system

--- src/main/java/Hotel_Resevation_System_Main.java
+++ src/main/java/Hotel_Resevation_System_Main.java
@@ -1,5 +1,39 @@
-public class Hotel_Resevation_System_Main {
-    public static void main(String[] args) {
-        System.out.println("welcome to hotel servation system");
+import java.util.ArrayList;
+import java.util.Scanner;
+//creating hotel1 class for first adding multiple hotels
+class Hotel
+{
+    //declearing data members as private for avoiding direct acess
+    private String name;
+    private int rateForRegularCustomer;
+    //parameterized constructor to initialised object
+    public Hotel(String name, int rateForRegularCustomer)
+    {
+        this.name=name;
+        this.rateForRegularCustomer=rateForRegularCustomer;
     }
+    // only implementing getter method to avoid unauthorized access
+    public String getName() {
+        return this.name;
+    }
+    public int getRateForRegularCustomer()
+    {
+        return this.rateForRegularCustomer;
+    }
+    // toString method for getting all the contents of current object
+    public String toString()
+    {
+        return "Hotel name is "+name+" rate for regular customer "+rateForRegularCustomer+"$";
+    }
+
+}
+public class Hotel_Resevation_System_Main {
+    static ArrayList<Hotel> hotel1 = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Hotel h=  new Hotel("Lakewood",110);
+        hotel1.add(h);
+        System.out.println(h);
+    }
+
 }
--- src/test/java/Hotel_Reservation_System_Test.java
+++ src/test/java/Hotel_Reservation_System_Test.java
@@ -1,10 +1,21 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Hotel_Reservation_System_Test {
 
+    // test case for cheking input is store or not
     @Test
-    void setup()
+    void shoudl_return_same_value_after_passing_throw_constructor()
     {
-        System.out.println("welcome to hotel reservation system test");
+        // given
+        String name = "akash";
+        int price=110;
+        //when
+        Hotel hotel1 = new Hotel(name,price);
+        String actualName=hotel1.getName();
+        int  actualPrice = hotel1.getRateForRegularCustomer();
+        //then
+        Assertions.assertEquals(actualName,name);
+        Assertions.assertEquals(actualPrice,price);
     }
 }
